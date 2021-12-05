dee6305a9b19aca376082d6728192291c909b220
Adnan Karim <44997411+adnankarim@users.noreply.github.com>
2021-12-05 17:56:56 +0500

    Create findRabbit.js

---
+++ findRabbit.js
@@ -0,0 +1,29 @@
+let pos =20;
+let len=1000;
+function step(){
+  let prev =pos;
+  if (Math.random()>0.5){
+    pos++;
+  }
+  else{
+    pos--;
+    
+  }
+  pos=Math.min(pos,len)
+  pos=Math.max(0,pos)
+  
+}
+let condition=true;
+let myCheck=Math.round(len/2);
+while (condition){
+  if (myCheck==pos)
+  {
+    console.log("found")
+    condition=false;
+  }
+  else{
+    step();
+  }
+}
+  
+  
