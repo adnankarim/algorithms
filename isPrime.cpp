ad06d4832e2d7d103787333d7cc9f63b09357e7f
Adnan Karim <44997411+adnankarim@users.noreply.github.com>
2022-08-30 18:42:11 +0500

    Removed library
    sqrt can be calc using square

--- isPrime.cpp
+++ isPrime.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
-#include <cmath>
+// #include <cmath>
 using namespace std;
 
 
 bool isPrime(int n)
 {   if (n<=1):
         return false;
-    for (int i=2; i<sqrt(n); i++)
+    for (int i=2; i*i<n; i++)
     {
         if(n%i==0)
         {
