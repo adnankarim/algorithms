76fa151d05f83ec20849d09489c0a0d439f9eda9
sacramento <skiwordylectures@gmail.com>
2022-09-26 18:22:04 +0500

    added reverse string  using Rescursion with space and time complexity

---
+++ recursion/reverseString.cpp
@@ -0,0 +1,20 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include <string>
+using namespace std;
+void reverseString(int i, string &arr,int n){
+    if (i>=n/2){
+        return;
+    }
+    swap(arr[i],arr[n-1-i]);
+    reverseString(i+1,arr,n);
+}
+int main() {
+    // Write C++ code here
+    
+    string arr ="adnan";
+    reverseString(0,arr,5);
+    for (int i=0;i<5;i++)
+        cout<<arr[i];
+    return 0;
+}
