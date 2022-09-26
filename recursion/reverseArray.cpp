c912b3538a018b8d9288bfc66682b3de7259fd5a
sacramento <skiwordylectures@gmail.com>
2022-09-26 18:14:53 +0500

    added reverse array  using Rescursion with space and time complexity

---
+++ recursion/reverseArray.cpp
@@ -0,0 +1,19 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+void reverseArray(int i, int arr[],int n){
+    if (i>=n/2){
+        return;
+    }
+    swap(arr[i],arr[n-1-i]);
+    reverseArray(i+1,arr,n);
+}
+int main() {
+    // Write C++ code here
+    
+    int arr [] ={1,2,3,4};
+    reverseArray(0,arr,4);
+    for (int i=0;i<4;i++)
+        cout<<arr[i];
+    return 0;
+}
